3ccb6dbc56f5845f9d138c024144ecaaa466732b
rimasultana <rimasultana20225@gmail.com>
2024-03-05 22:36:47 +0600

    condition

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 18;
+
+
+if(age < 18) {
+    console.log("Your are young!");
+} else if(age == 18) {
+console.log("Welcome adult!");
+} else if(age > 18 && age < 30){
+    console.log("Your are machud!");
+} else{
+    console.log("ypu are old");
+}
+
+console.log(age);
